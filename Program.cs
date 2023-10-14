6a3ef21d15c087777fec8516fdac92a66d10074c
Anthony Alexander Atama <100134398+alexanderatama1@users.noreply.github.com>
2023-10-14 22:40:58 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿decimal triangleBase, triangleHeight;
+
+// Prompt and store base
+Console.Write("Enter triangle base: ");
+triangleBase = decimal.Parse(Console.ReadLine());
+
+// Prompt and store height
+Console.Write("Enter triangle height: ");
+triangleHeight = decimal.Parse(Console.ReadLine());
+
+// Print area of rectangle
+Console.WriteLine("The area of the triangle is: " + (0.5M * triangleBase * triangleHeight));
